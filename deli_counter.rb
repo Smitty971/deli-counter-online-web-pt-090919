c021052118d2aca1a9a9de1480a71ea620c18f94
Jamal Turner <jlsmitty2273@yahoo.com>
2019-09-19 20:30:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 def line(array)
   if array == []
-  puts "The line is currently empty."
+    puts "The line is currently empty."
  elsif phrase = "The line is currently:"
-  array.each_with_index do |name, index|
-  phrase = phrase + " #{index + 1}. #{name}"
-  end
-  puts phrase
+   array.each_with_index do |name, index|
+      phrase = phrase + " #{index + 1}. #{name}"
+    end
+    puts phrase
   end
 
 end
