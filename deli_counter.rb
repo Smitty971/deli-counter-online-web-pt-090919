781356a1a0a6f6c41a31d36f56881caa13562bf7
Jamal Turner <jlsmitty2273@yahoo.com>
2019-09-19 19:55:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ def take_a_number(array, name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
   end
 
-def now_serving(array)
+def now_serving(name)
   if array == []
     puts "There is nobody waiting to be served!"
   else
